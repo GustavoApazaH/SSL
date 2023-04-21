9abf0598d308d5a65057fb44d5ed8d25c7508941
Gustavo Apaza Huanca <gustavoapazah@gmail.com>
2023-04-20 23:50:41 -0300

    Agrego los archivos hello y output

---
+++ 00-CHelloworld/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	char mensaje[12] = "Hola Mundo!";
+	//char mensajePorTeclado[20];
+	
+	printf("Primer mensaje: %s \n", mensaje);
+	//printf("\nEscribe una palabra: ");
+	//scanf("%s", mensajePorTeclado);
+	//printf("\nTu mensaje es: %s", mensajePorTeclado);
+	
+	return 0;
+}
